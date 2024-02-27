bc02dde81c2b9d3978629a7f87a4bef0a1c1eb2c
Gimyoeun <gimhyoeun80@gmail.com>
2024-02-27 17:46:22 +0900

    fix : 현재 기온 화면 코드 변경

--- components/CurrTemp.tsx
+++ components/CurrTemp.tsx
@@ -17,10 +17,10 @@ const CurrTemp = (props: { tempt: temperatureStateType }) => {
         <>
             <div className="align-baseline flex flex-col pt-8 h-64 w-96 mx-auto bg-white opacity-40 items-center">
                 {!isNaN(tmp) && (
-                    <h1 className="text-3xl flex flex-col text-green-950 opacity-100 subpixel-antialiased font-bold">{`현재 기온 : ${tmp}${NCST_DATA['T1H'].unit}`}</h1>
+                    <h1 className="text-3xl flex flex-col text-green-950 opacity-100 subpixel-antialiased font-bold">{`현재 기온은 ${tmp}${NCST_DATA['T1H'].unit}`}</h1>
                 )}
                 <div className="text-[13px] flex items-center mt-0 pt-2 font-bold">
-                    {pty == '0' && (
+                    {pty !== '0' && (
                         <>
                             <p>{`${ptyStr} 이/가 와요 `}</p>
                         </>
@@ -32,10 +32,10 @@ const CurrTemp = (props: { tempt: temperatureStateType }) => {
                     {!isNaN(wsd) && <p className="ml-2">{`풍속 : ${wsd}${NCST_DATA['WSD'].unit}`}</p>}
                 </div>
                 <div className="text-[13px] flex items-center mt-0 pt-2 font-bold">
-                    {!isNaN(pop) && <p>{`강수확률 : ${pop}${NCST_DATA['POP'].unit} | `}</p>}
-                    {pty == '0' && (
+                    {!isNaN(pop) && <p>{`강수확률 : ${pop}${NCST_DATA['POP'].unit}`}</p>}
+                    {pty !== '0' && (
                         <>
-                            <p className="ml-2">{`강수량 : ${pcp}${NCST_DATA['RN1'].unit}`}</p>
+                            <p className="ml-2">{` | 강수량 : ${pcp}${NCST_DATA['RN1'].unit}`}</p>
                         </>
                     )}
                 </div>
